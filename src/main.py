4543af03f9c9d01a68634ae0f8cd3f805d707018
Kenzo Wada <used.acormhib@gmail.com>
2023-03-19 23:52:16 +0900

    :art: update structure

--- src/main.py
+++ src/main.py
@@ -1,53 +1,39 @@
-import pyaudio
-import speech_recognition as sr
-import openai
-import pyttsx3
 import os
+from speech_recognizer import SpeechRecognizer
+from openai_api import OpenAIAPI
+from text_to_speech import TextToSpeech
 
-# Text to speechエンジンの初期化
-engine = pyttsx3.init()
 
 # 環境変数からAPIキーを取得
-openai.api_key = os.environ.get('OPENAI_API_KEY')
+openai_api_key = os.environ.get('OPENAI_API_KEY')
 
-# 音声入力用の設定
-CHUNK = 1024
-FORMAT = pyaudio.paInt16
-CHANNELS = 1
-RATE = 44100
+# 音声認識の設定
+recognizer = SpeechRecognizer()
 
-# 音声入力用のオブジェクトの生成
-p = pyaudio.PyAudio()
-stream = p.open(format=FORMAT, channels=CHANNELS, rate=RATE, input=True, frames_per_buffer=CHUNK, input_device_index=2)
+# OpenAI APIの設定
+openai_api = OpenAIAPI(api_key=openai_api_key, engine="text-davinci-002", max_tokens=1024, temperature=0.5)
 
-# 音声入力からテキストへの変換用のオブジェクトの生成
-r = sr.Recognizer()
+# Text-to-Speechの設定
+tts = TextToSpeech()
 
-while True:
+# 音声入力から回答までの処理
+def process_input():
     # 音声入力の取得
-    data = stream.read(CHUNK)
+    data = recognizer.get_audio_input()
     
     # 音声入力からテキストへの変換
-    try:
-        text = r.recognize_google(data)
-        print("Input:", text)
-    except sr.UnknownValueError:
-        text = ""
-    except sr.RequestError:
-        text = ""
+    text = recognizer.recognize_google(data)
+    print("Input:", text)
     
-    # GPT-4による回答の生成
-    try:
-        response = openai.Completion.create(engine="text-davinci-002", prompt=text, max_tokens=1024, n=1, stop=None, temperature=0.5)
-        answer = response.choices[0].text
-        print("Response:", answer)
-    except openai.error.APIError as e:
-        print("Error: ", e)
-        answer = "Sorry, I could not generate a response."
+    # OpenAI APIによる回答の生成
+    answer = openai_api.generate_answer(text)
+    print("Response:", answer)
     
     # 回答の読み上げ
-    try:
-        engine.say(answer)
-        engine.runAndWait()
-    except pyttsx3.exceptions.EngineError as e:
-        print("Error: ", e)
+    tts.speak(answer)
+
+
+# メイン関数
+if __name__ == '__main__':
+    while True:
+        process_input()
